ce77347d39b55acfbb7804202bce7474f4c9a891
Håvard Wormdal Høiby <havardwhoiby@gmail.com>
2013-06-11 13:00:15 +0200

    Added comment and refactored

--- tip-of-the-day.el
+++ tip-of-the-day.el
@@ -1,37 +1,49 @@
-(defconst tip-buffer-name "*tip-of-the-day*")
-(defconst tip-file-name "/Users/havard/Documents/programmering/elisp/tip-of-the-day/test-file")
+; Tip of the day
+;
+; Shows a new tip of the day for each time emacs is opened.
+;
+; Usage:
+;   Append this to the end of your .emacs file
+;
+;   (load-file "<path-to-this-file>")
+;   (require 'tip-of-the-day)
+;   (show-and-rotate-tip-of-the-day "<path-to-tip-file>")
 
-(defun rotate (list)
-  (append (cdr list) (cons (car list) nil)))
+(defconst tip-buffer-name "*tip-of-the-day*")
+
+(defun show-and-rotate-tip-of-the-day (file)
+  "Opens a buffer with the tip of the day and rotates the tip file"
+  (interactive)
+  (split-window)
+  (switch-to-buffer (get-buffer-create tip-buffer-name))
+  (with-current-buffer tip-buffer-name
+    (insert (read-first-line file)))
+  (rotate-file file))
+
+(defun read-first-line (file)
+  "Read the first line from a file"
+  (car (read-lines file)))
 
 (defun rotate-file (file)
+  "Rotates a file"
   (let ((lines (read-lines file)))
     (delete-file file)
     (write-lines file (rotate lines))))
 
 (defun read-lines (file)
   "Return file as list of lines"
-  (interactive)
   (with-temp-buffer
     (insert-file-contents file)
     (split-string (buffer-string) "\n" t)))
 
+(defun rotate (list)
+  "Rotates a list left"
+  (append (cdr list) (cons (car list) nil)))
+
 (defun write-lines (file lines)
   "Writes the lines to a file"
-  (interactive)
   (when lines
     (append-to-file (concat (car lines) "\n") nil file)
     (write-lines file (cdr lines))))
-
-(defun read-first-line (file)
-  (car (read-lines file)))
-
-(defun show-tip-of-the-day nil
-  (interactive)
-  (split-window)
-  (switch-to-buffer (get-buffer-create tip-buffer-name))
-  (with-current-buffer tip-buffer-name
-    (insert (read-first-line tip-file-name)))
-  (rotate-file tip-file-name))
 
 (provide 'tip-of-the-day)
